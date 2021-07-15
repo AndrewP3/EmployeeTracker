5902fa30ab61f882c53629b182362501acfc2a5b
AndrewP3 <andrewpowell097@gmail.com>
2021-07-15 15:35:34 -0700

    3rd commit

--- index.js
+++ index.js
@@ -11,5 +11,45 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  runSearch();
+  runmenu();
 });
+
+function menu() {
+  inquirer
+    .prompt({
+      name: "select",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role",
+      ],
+    })
+
+    .then(function (answer) {
+      console.log(answer);
+
+      if (answer.select === "View All Employees") {
+        viewAll();
+      } else if (answer.select === "View Department") {
+        viewDepartments();
+      } else if (answer.select === "View role") {
+        viewRoles();
+      } else if (answer.select === "Add Employee") {
+        addEmployee();
+      } else if (answer.select === "Add department") {
+        addDepartment();
+      } else if (answer.select === "Add Role") {
+        addRole();
+      } else if (answer.select === "Update Role") {
+        updateRole();
+      } else {
+        connection.end();
+      }
+    });
+}
